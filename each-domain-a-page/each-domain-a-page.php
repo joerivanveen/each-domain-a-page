ea73cc0391bbf8f5bccb14a749c189331601b559
Joeri van Veen <nonstockphoto@gmail.com>
2019-09-13 10:37:14 +0200

    Initial setup and check if (g)it works

---
+++ each-domain-a-page/each-domain-a-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Each domain a page
+Plugin URI: https://github.com/joerivanveen/each-domain-a-page
+Description: Serves a specific page from Wordpress depending on the domain used to access the Wordpress installation.
+Version: 0.0.1
+Author: Ruige hond
+Author URI: https://ruigehond.nl
+License: GPL3
+*/
+
+defined( 'ABSPATH' ) or die();
