44b8960191ba9e9cdbb59551124d2cb84324cd57
Joeri van Veen <nonstockphoto@gmail.com>
2019-09-13 12:48:41 +0200

    Proof of concept with redirect (header:location) to individual page

--- each-domain-a-page/each-domain-a-page.php
+++ each-domain-a-page/each-domain-a-page.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Each domain a page
 Plugin URI: https://github.com/joerivanveen/each-domain-a-page
@@ -7,7 +6,37 @@ Description: Serves a specific page from Wordpress depending on the domain used 
 Version: 0.0.1
 Author: Ruige hond
 Author URI: https://ruigehond.nl
-License: GPL3
+License: GPLv3
+Text Domain: ruigehond
+Domain Path: /languages
 */
-
-defined( 'ABSPATH' ) or die();
+defined('ABSPATH') or die();
+// This is plugin nr. 7 by Ruige hond. It identifies as: ruigehond007.
+Define('RUIGEHOND007_VERSION', '0.0.1');
+// Register hooks for plugin management, functions are at the bottom of this file.
+//register_activation_hook(__FILE__, 'ruigehond007_install');
+//register_deactivation_hook(__FILE__, 'ruigehond007_deactivate');
+//register_uninstall_hook(__FILE__, 'ruigehond007_uninstall');
+// Startup the plugin
+//add_action('init', 'ruigehond007_run');
+add_action('wp', 'ruigehond007_powerrr');
+function ruigehond007_powerrr()
+{
+    $domain = $_SERVER['HTTP_HOST'];
+    // TODO cleanup subdomain from domain?
+    $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . '://' . $domain;
+    $slug = str_replace('.', '_', $domain);
+    $posts = get_posts(array(
+        'name' => $slug,
+        'post_type' => 'page',
+        'post_status' => 'publish',
+        'posts_per_page' => 1
+    ));
+    if ($posts):
+        $GLOBALS['post'] = $posts[0];
+        if (str_replace('/', '', $_SERVER['REQUEST_URI']) !== $slug) {
+            header('Location: ' . $url . '/' . $slug, '');
+            die();
+        }
+    endif;
+}
